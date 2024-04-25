ede63bbe781d1422f202bba33dbeb4cb08a6dea2
bilalahmedkhatri <bilalahmedkhatri@outlook.com>
2024-04-25 06:52:54 +0200

    working on url

--- src/layouts/video-player/components/index.js
+++ src/layouts/video-player/components/index.js
@@ -14,33 +14,49 @@ Coded by www.azeemlab.com
 
 import { Button } from "@mui/material";
 import React, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 import ReactPlayer from "react-player"; // for video player controls
 
 const VideoFramePlayer = () => {
+  const uid = require("uuid");
   const [frameData, setFrameData] = useState(null);
   // connection successfully with "localhost"
   // create data straucture to handle data on server to allow receive data as per button click
   // most important receive data from server to play video //////// not tested yet with frames {ruf concept}.
 
   const [sendData, setSendData] = useState(null);
-  const APIData = "aashaaz_OS10";
+  const v = uid.v4();
+  const APIData = v;
+  // const APIData = "aashaaz_OS10";
+  // useEffect(() => {
+  //   // WebSocket logic to connect and receive frames
+  //   // const url = `ws://192.168.1.85:8006/${APIData}/web`;
+  //   const url = `ws://localhost:8006/${APIData}/web`;
+  //   const ws = new WebSocket(url);
+  //   // ws.addEventListener("message", (event) => setFrameData(event.data)); // Update frame data
+  //   // ws.onmessage = (event) => console.log("result data", event.data); // Update frame data
 
+  //   ws.addEventListener("open", (event) => {
+  //     ws.send("Hello Server!");
+  //   });
+  //   ws.addEventListener("message", (event) => {
+  //     console.log("Message from server ", event.data);
+  //   });
+  //   return () => ws.close(); // Clean up WebSocket on unmount
+  // }, []);
   useEffect(() => {
-    // WebSocket logic to connect and receive frames
-    // const url = `ws://192.168.1.85:8006/${APIData}/web`;
-    const url = `ws://localhost:8006/${APIData}/web`;
-    const ws = new WebSocket(url);
-    // ws.addEventListener("message", (event) => setFrameData(event.data)); // Update frame data
-    // ws.onmessage = (event) => console.log("result data", event.data); // Update frame data
+    const socket = io(`ws://localhost:8006/${APIData}/web`); // Replace with your server URL
 
-    ws.addEventListener("open", (event) => {
-      ws.send("Hello Server!");
+    socket.on("connect", () => {
+      console.log("Connected to server!");
     });
-    ws.addEventListener("message", (event) => {
-      console.log("Message from server ", event.data);
+
+    socket.on("message", (data) => {
+      console.log("Received message:", data);
     });
-    return () => ws.close(); // Clean up WebSocket on unmount
-  }, []);
+
+    socket.emit("send-message", "Hello from the client!");
+  });
 
   const videoSource = frameData ? `data:image/jpeg;base64,${frameData}` : null;
 
